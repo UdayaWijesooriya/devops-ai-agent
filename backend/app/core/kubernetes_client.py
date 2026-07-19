13d833441073a06b11c16d12c6404d118d28d3ee
UdayaWijesooriya <udayaswijesooriya@gmail.com>
2026-07-20 00:05:47 +0530

    Changed funtion name for future and add kubernetes abstraction layer

--- backend/app/core/kubernetes_client.py
+++ backend/app/core/kubernetes_client.py
@@ -1,19 +1,26 @@
 from kubernetes import client, config
 
 class KubernetesClient:
-    
+
     def __init__(self):
         # Load the kubernetes configuration from the defult location
-        self._load_kubernetes_config()
+        self._initialize_client()
 
-    def _load_kubernetes_config(self):
+    def _initialize_client(self):
         # Load kubernetes configuration from the defult location
         config.load_kube_config()
         self.core_api = client.CoreV1Api()
 
     def get_pods(self, namespace):
         # get pods from the kubernetes cluster
-        pods = self.core_api.list_namespaced_pod(
-            namespace=namespace
-        )
-        return pods.items
+        try:
+            pods = self.core_api.list_namespaced_pod(
+                namespace=namespace
+            )
+            
+            return pods.items
+        
+        except Exception as error:
+            print(f"Error fetching pods: {error}")
+            return []
+        
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,25 @@
-from app.core.kubernetes_client import KubernetesClient
+from app.tools.kubernetes_tool import KubernetesTool
 
 def main():
 
-    kubernetes_client = KubernetesClient()
+    kubernetes_tool = KubernetesTool()
 
-    pods = kubernetes_client.get_pods(
+    pods = kubernetes_tool.get_pods(
         namespace="demo"
     )
 
     for pod in pods:
 
         print(
-            f"Pod Name: {pod.metadata.name}"
+            f"Pod Name: {pod['name']}"
         )
 
         print(
-            f"Pod Status: {pod.status.phase}"
+            f"Pod Status: {pod['status']}"
         )
 
         print(
-            f"Pod Node: {pod.spec.node_name}"
+            f"Pod Node: {pod['node']}"
         )
 
 
